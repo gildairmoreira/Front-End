d28f0fe7c0c1c2c50776240bf37e4e5caba2f5e2
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2022-10-31 19:42:09 -0300

    inicio de RegEx

---
+++ JavaScriptWEB/7daysOfCode/desafios/d02/main.js
@@ -0,0 +1,19 @@
+let nome = window.prompt('Qual seu nome')
+let idade = window.prompt('Quantos anos você tem?')
+let linguagem = window.prompt('Qual linguagem de programação você está estudando?')
+
+document.write(`Olá ${nome}, Você tem ${idade} de idade e já está aprendendo ${linguagem}`)
+
+let estudo = window.prompt(`Você gosta de Estudar JavaScript? Responda com 1 para SIM OU 2 PARA NÃO`)
+
+if (estudo == 1 || estudo == 2){
+    if (estudo == 1){
+        document.write(`Muito bom! Continue estudando e você terá muito sucesso.`)
+    }
+    if (estudo == 2){
+        document.write(`Ahh que pena... Já tentou aprender outras linguagens?`)
+    }
+}  else{
+    document.write(`Opção invalida`)
+}
+
