e90f566dc3c2195433df65f445ee2b586a469d47
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2022-12-14 20:06:35 -0300

    metade do curso de DOM

---
+++ Js-para-Front-end/robotron-2000/js/main.js
@@ -0,0 +1,9 @@
+const robotron = document.querySelector("#robotron")
+
+robotron.addEventListener("click", () => {
+    console.log("cliquei no Robô.")
+})
+
+function dizOI (nome){
+    console.log('oi meu chapa')
+}
