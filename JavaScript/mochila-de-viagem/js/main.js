faf3999670e07f02acd94d007d400fa4c73fa11f
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2022-12-16 22:19:40 -0300

    começando o guardando dados

---
+++ JavaScript/mochila-de-viagem/js/main.js
@@ -0,0 +1,21 @@
+const form = document.getElementById("novoItem")
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+
+    console.log(evento)
+    criaElemento(evento.target.elements[ 'nome' ].value, evento.target.elements[ 'quantidade' ].value)
+})
+
+function criaElemento(nome, quantidade) {
+    console.log(nome)
+    console.log(quantidade)
+
+    const novoItem = document.createElement('li')
+    novoItem.classList.add('item')
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = quantidade
+
+    console.log(numeroItem)
+}
