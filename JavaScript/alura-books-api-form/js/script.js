a64f74c7bd578ccdc955ab679fe24a01afb20834
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2022-12-27 19:47:09 -0300

    1

--- JavaScript/alura-books-api-form/js/script.js
+++ JavaScript/alura-books-api-form/js/script.js
@@ -4,9 +4,21 @@ async function buscaEndereco(cep)
     {
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
         var consultaCEPConvertida = await consultaCEP.json()
+        
         if(consultaCEPConvertida.erro){
             throw Error ('CEP não Existe!')
         }
+
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+        var bairro = document.getElementById('bairro');
+
+        bairro.value = consultaCEPConvertida.bairro;
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
     } catch(erro){
