8c4a37a9c2b2c5a664a12cef4ba5bda396f96a0b
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2022-12-27 17:42:46 -0300

    cep

--- JavaScript/alura-books-api-form/js/script.js
+++ JavaScript/alura-books-api-form/js/script.js
@@ -1,3 +1,19 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+async function buscaEndereco()
+{
+    try
+    {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        var consultaCEPConvertida = await consultaCEP.json()
+        if(consultaCEPConvertida.erro){
+            throw Error ('CEP não Existe!')
+        }
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch(erro){
+        console.log(erro);
+    }
+}
 
-console.log(consultaCEP);
+let ceps = ['01001000','35835000']
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores))
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas))
