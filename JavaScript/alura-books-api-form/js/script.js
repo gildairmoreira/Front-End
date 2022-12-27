806984ff180edae1e029bc3f61bef7ddf74077ab
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2022-12-27 19:59:24 -0300

    terminado a validação de cep

--- JavaScript/alura-books-api-form/js/script.js
+++ JavaScript/alura-books-api-form/js/script.js
@@ -1,5 +1,7 @@
 async function buscaEndereco(cep)
 {
+    var mensagemErro = document.getElementById('erro')
+    mensagemErro.innerHTML = "";
     try
     {
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
@@ -22,6 +24,7 @@ async function buscaEndereco(cep)
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
     } catch(erro){
+        mensagemErro.innerHTML = `<p>CEP invalído Tente novamente!</p>`
         console.log(erro);
     }
 }   
