930625b033b918689910904ca0d4c681b0279617
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-07-21 16:26:21 -0300

    post

--- JavaScript-Requesiçoes/pet-shop-CRUD/admin/service/cliente-service.js
+++ JavaScript-Requesiçoes/pet-shop-CRUD/admin/service/cliente-service.js
@@ -1,9 +1,32 @@
-export const listaClientes = () => {
+const listaClientes = () => {
   return fetch(`http://localhost:3000/profile`).then((resposta) => {
     return resposta.json();
   });
 };
 
+const criaCliente = (nome, email) => {
+  return fetch(`http://localhost:3000/profile`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      nome: nome,
+    }),
+  }).then((resposta) => {
+    return resposta.body;
+  });
+};
+
+const removeCliente = (id) => {
+  return fetch(`http://localhost:3000/profile/${id}`, {
+    method: "DELETE",
+  })
+}
+
 export const clienteService = {
   listaClientes,
+  criaCliente,
+  removeCliente
 };
