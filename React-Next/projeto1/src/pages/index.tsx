a31993e1c3addb4ef36135211d08ece10683fea4
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-10-26 20:46:10 -0300

    aula 4

--- React-Next/projeto1/src/pages/index.tsx
+++ React-Next/projeto1/src/pages/index.tsx
@@ -2,11 +2,23 @@
 // import { Inter } from 'next/font/google'
 
 // const inter = Inter({ subsets: ['latin'] })
+const canal = 'CFBCursos'
+const nome = 'Bruno Campos'
+
+function Topo() {
+  return(
+    <div>
+      <div>Logo</div>
+      <div>{canal}</div>
+      <div>{nome}</div>
+    </div>
+  )
+}
 
 export default function Home() {
   return (
     <main>
-     
+    <Topo/>
     </main>
   )
 }
